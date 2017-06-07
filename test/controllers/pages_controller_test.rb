993545f3323a3a58b77e2dbabddb124738bb440f
Ouissal Benameur <you@example.com>
2017-06-07 17:50:24 +0000

    added some tests

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,49 +1,68 @@
 require 'test_helper'
 
 class PagesControllerTest < ActionController::TestCase
+
+
+  def setup
+    @base_title = "MOOC"
+  end
+
+
+
+
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get courses" do
     get :courses
     assert_response :success
+    assert_select "title", "Courses | #{@base_title}"
   end
 
   test "should get programs" do
     get :programs
     assert_response :success
+        assert_select "title", "Programs | #{@base_title}"
   end
 
   test "should get schools" do
     get :schools
     assert_response :success
+        assert_select "title", "Schools | #{@base_title}"
+
   end
 
   test "should get dashboard" do
     get :dashboard
     assert_response :success
+        assert_select "title", "Dashboard | #{@base_title}"
   end
 
   test "should get profile" do
     get :profile
     assert_response :success
+    assert_select "title", "Profile | #{@base_title}"
   end
 
   test "should get account" do
     get :account
     assert_response :success
+    assert_select "title", "Account | #{@base_title}"
   end
 
   test "should get signout" do
     get :signout
     assert_response :success
+    assert_select "title", "Signout | #{@base_title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | #{@base_title}"
   end
 
 end
