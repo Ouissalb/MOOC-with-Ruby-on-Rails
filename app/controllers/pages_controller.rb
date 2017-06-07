56f05f989401ba2f08006b85b637953593998287
Ouissal Benameur <you@example.com>
2017-06-07 17:03:53 +0000

    just started

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,28 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def courses
+  end
+
+  def programs
+  end
+
+  def schools
+  end
+
+  def dashboard
+  end
+
+  def profile
+  end
+
+  def account
+  end
+
+  def signout
+  end
+
+  def help
+  end
+end
