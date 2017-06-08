4104581846c3de8edf41e2516227438461a6dec8
Ouissal Benameur <you@example.com>
2017-06-08 13:45:34 +0000

    added user password and confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,23 @@
 class User < ActiveRecord::Base
+
+  #Regular expression for validating emails
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  #To save all emails in downcase
+  before_save { self.email = email.downcase }
+
+  #Validating name :
+  validates :name,  presence: true, length: { maximum: 50 }
+
+  #Validating email :
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+
+  #Validating password
+  validates :password, presence: true, length: { minimum: 6 }
+
+
+  has_secure_password
+
 end
