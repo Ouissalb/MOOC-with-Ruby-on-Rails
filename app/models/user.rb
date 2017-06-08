5facf6a84b92ef614be6aee1d7449a1e380bcda4
Ouissal Benameur <you@example.com>
2017-06-08 21:27:06 +0000

    some edits ..

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
 
+  attr_accessor :remember_token
+
+
   #Regular expression for validating emails
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
@@ -19,11 +22,35 @@ class User < ActiveRecord::Base
 
 
   has_secure_password
-  
-    def User.digest(string)
+
+    class << self
+
+      def digest(string)
         cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                  BCrypt::Engine.cost
+                                                      BCrypt::Engine.cost
         BCrypt::Password.create(string, cost: cost)
+      end
+
+      # Returns a random token.
+      def new_token
+        SecureRandom.urlsafe_base64
+      end
     end
+
+    # Remembers a user in the database for use in persistent sessions.
+    def remember
+      self.remember_token = User.new_token
+      update_attribute(:remember_digest, User.digest(remember_token))
+    end
+
+    # Returns true if the given token matches the digest.
+   def authenticated?(remember_token)
+     BCrypt::Password.new(remember_digest).is_password?(remember_token)
+   end
+
+   # Forgets a user.
+   def forget
+     update_attribute(:remember_digest, nil)
+   end
 
 end
